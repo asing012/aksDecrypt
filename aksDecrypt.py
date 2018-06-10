f6af38caf248e2a92e2e94ed73b8f920dcfdd94c
Akshay Singh <akshayc_118@hotmail.com>
2018-06-10 15:40:55 -0700

    First commit

---
+++ aksDecrypt.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Akshay Singh
+# Decyrpts DES hash(should include SALT) using dictionary attack
+
+import crypt
+
+def testPass(encryptText): # to decrypt pass  
+    passSalt = encryptText[0:2] # 2 characters to get the salt
+    dictFile = open('dictionary.txt','r')
+    for word in dictFile.readlines():
+        word = word.strip('\n')
+        dictCrypt = crypt.crypt(word,passSalt)
+        if (dictCrypt == encryptText):
+            print("\t\t "+"Password Found: "+word)
+            return
+    print("\t\t "+"Password Not Found")
+    return
+
+def main():
+    passFile = open('encryptPass.txt') 
+    print("\nWelcome to aksDecrypt \n")
+    for password in passFile.readlines():
+        if ":" in password: #if the txt file has user:password
+            user = password.split(':')[0]
+            encryptText = password.split(':')[1].strip('\n')
+            print("[+] "+ "Decrypting password for: "+user)
+            testPass(encryptText)
+        else: #if the txt file doesn't have user:password
+            encryptText = password.strip('\n')
+            print("[+] "+ "Decrypting password for Unknown User: ")
+            testPass(encryptText)
+
+if __name__ == "__main__":
+    main()
